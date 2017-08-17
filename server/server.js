1cae3b500658095d5132e6973d2d56ef7db71072
Orion <orion@Jon-MacBook-Pro.local>
2017-08-17 12:35:25 +0100

    Server is now fixed

--- server/server.js
+++ server/server.js
@@ -6,10 +6,8 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
-var server = app.listen(process.env.PORT || 3000, function () {
-  const port = server.address().port;
-  console.log("Express is working on port " + port);
-});
+var app = Express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
