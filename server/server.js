a221cb1defbd9f6afd759d53f9f4c3f89441e558
Orion <orion@Jon-MacBook-Pro.local>
2017-08-17 15:27:39 +0100

    Server is now fixed

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
-var app = Express();
+var app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
